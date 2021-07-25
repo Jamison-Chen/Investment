6974099d44b386f9c6e3594b1c12498edda49d0b
Jamison Chen <jamisonforeverandever@gmail.com>
2021-07-25 14:09:06 +0800

    fix some bugs in stockInfo view in the backend

--- backend/investment/stockInfoScraper/stockInfo.py
+++ backend/investment/stockInfoScraper/stockInfo.py
@@ -28,10 +28,11 @@ class StockInfoView:
             return int(StockInfo.objects.all()[0].date)
         return 0
 
-    def fetchAndStore(self, sidList, mode):
+    def fetchAndStore(self, sidList):
         try:
             allData = []
             for each in sidList:
+                # fetch
                 try:
                     res = get(self.endPoint12 + "tse_" + each + ".tw")
                     res = json.loads(pq(res.text).text())["msgArray"][0]
@@ -42,55 +43,45 @@ class StockInfoView:
                     except:
                         print("failed to fetch %s" % each)
                         continue
+                # arrange the data format
                 dataRow = {}
-                dataRow["date"] = res["d"].split('.')[0]
-                dataRow["sid"] = res["ch"].split('.')[0]
-                dataRow["name"] = res["n"]
-                dataRow["trade-type"] = res["ex"]
-                dataRow["quantity"] = res["v"]
-                dataRow["open"] = str(round(float(res["o"]), 2))
-                try:  # 漲停板時，z 會是 "-"，所以改看最高價
-                    dataRow["close"] = str(round(float(res["z"]), 2))
+                try:
+                    dataRow["date"] = res["d"].split('.')[0]
+                    dataRow["sid"] = res["ch"].split('.')[0]
+                    dataRow["name"] = res["n"]
+                    dataRow["trade-type"] = res["ex"]
+                    dataRow["quantity"] = res["v"]
+                    dataRow["open"] = str(round(float(res["o"]), 2))
+                    try:  # 收漲停時，z 會是 "-"，所以改看最高價
+                        dataRow["close"] = str(round(float(res["z"]), 2))
+                    except:
+                        dataRow["close"] = str(round(float(res["h"]), 2))
+                    dataRow["highest"] = str(round(float(res["h"]), 2))
+                    dataRow["lowest"] = str(round(float(res["l"]), 2))
+                    dataRow["fluct-price"] = str(
+                        round((float(dataRow["close"])-float(res["y"])), 2))
+                    dataRow["fluct-rate"] = str(
+                        round((float(dataRow["close"])-float(res["y"]))/float(res["y"]), 4))
                 except:
-                    dataRow["close"] = str(round(float(res["h"]), 2))
-                dataRow["highest"] = str(round(float(res["h"]), 2))
-                dataRow["lowest"] = str(round(float(res["l"]), 2))
-                dataRow["fluct-price"] = str(
-                    round((float(dataRow["close"])-float(res["y"])), 2))
-                dataRow["fluct-rate"] = str(
-                    round((float(dataRow["close"])-float(res["y"]))/float(res["y"]), 4))
+                    continue
                 allData.append(dataRow)
-            if mode == "refreshInfo":
-                for each in allData:
-                    q = StockInfo.objects.filter(sid=each["sid"])
-                    if len(q) != 0:
-                        q = q.get()
-                        q.date = each["date"]
-                        q.companyName = each["name"]
-                        q.tradeType = each["trade-type"]
-                        q.quantity = each["quantity"]
-                        q.openPrice = each["open"]
-                        q.closePrice = each["close"]
-                        q.highestPrice = each["highest"]
-                        q.lowestPrice = each["lowest"]
-                        q.fluctPrice = each["fluct-price"]
-                        q.fluctRate = each["fluct-rate"]
-                        q.save()
-                    else:
-                        s = StockInfo(date=each["date"],
-                                      sid=each["sid"],
-                                      companyName=each["name"],
-                                      tradeType=each["trade-type"],
-                                      quantity=each["quantity"],
-                                      openPrice=each["open"],
-                                      closePrice=each["close"],
-                                      highestPrice=each["highest"],
-                                      lowestPrice=each["lowest"],
-                                      fluctPrice=each["fluct-price"],
-                                      fluctRate=each["fluct-rate"])
-                        s.save()
-            elif mode == "append":
-                for each in allData:
+            # store
+            for each in allData:
+                q = StockInfo.objects.filter(sid=each["sid"])
+                if len(q) != 0:
+                    q = q.get()
+                    q.date = each["date"]
+                    q.companyName = each["name"]
+                    q.tradeType = each["trade-type"]
+                    q.quantity = each["quantity"]
+                    q.openPrice = each["open"]
+                    q.closePrice = each["close"]
+                    q.highestPrice = each["highest"]
+                    q.lowestPrice = each["lowest"]
+                    q.fluctPrice = each["fluct-price"]
+                    q.fluctRate = each["fluct-rate"]
+                    q.save()
+                else:
                     s = StockInfo(date=each["date"],
                                   sid=each["sid"],
                                   companyName=each["name"],
@@ -103,7 +94,7 @@ class StockInfoView:
                                   fluctPrice=each["fluct-price"],
                                   fluctRate=each["fluct-rate"])
                     s.save()
-            self.result.extend(allData)
+            self.prepareResult(sidList)
         except Exception as e:
             raise e
 
@@ -116,17 +107,14 @@ class StockInfoView:
         date = int(date)
         try:
             if self.recordedDate != date:
-                self.fetchAndStore(sidList, "refreshInfo")
+                self.fetchAndStore(sidList)
                 self.recordedDate = date
+            else:
+                self.prepareResult(sidList)
         except Exception as e:
             raise e
-        self.getInfo(sidList)
 
-    def getInfo(self, sidList=[]):
-        newSid = []
-        # q = StockInfo.objects.all()
-        # existingSid = [each.sid for each in q]
-        # newSid = list(set(sidList)-set(existingSid))
+    def prepareResult(self, sidList):
         for eachSid in sidList:
             q = StockInfo.objects.filter(sid=eachSid)
             if len(q) != 0:
@@ -143,7 +131,5 @@ class StockInfoView:
                                     "fluct-price": q.fluctPrice,
                                     "fluct-rate": q.fluctRate})
             else:
-                newSid.append(eachSid)
-        if len(newSid) != 0:
-            # the function below will also append these new stock info into "result"
-            self.fetchAndStore(newSid, "append")
+                raise Exception(
+                    "Error in prepareResult: failed to get info of %s" % eachSid)
